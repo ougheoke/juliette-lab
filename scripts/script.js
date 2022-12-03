2d2111091bec30ee5f69587834cd69840009d583
Omonegho Ugheoke <111533969+ougheoke@users.noreply.github.com>
2022-12-03 16:36:44 -0600

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,54 @@
+$(document).ready(function () {
+    //click on a course, display the corresponding course tab/page
+  
+    $("#course1").click(function () {
+      $(".course-div").hide();
+  
+      console.log("User clicked on Course 1.");
+      $("#output-image1").show();
+    });
+  
+    $("#course2").click(function () {
+      $(".course-div").hide();
+      console.log("User clicked on Course 2.");
+      $("#output-image2").show();
+    });
+  
+    $("#course3").click(function () {
+        $(".course-div").hide();
+        console.log("User clicked on Course 3.");
+        $("#output-image3").show();
+    });
+  
+    $("#course4").click(function () {
+        $(".course-div").hide();
+        console.log("User clicked on Course 4.");
+        $("#output-image4").show();
+    });
+
+    $("#course5").click(function () {
+        $(".course-div").hide();
+        console.log("User clicked on Course 5.");
+        $("#output-image5").show();
+      });
+
+      $("#course6").click(function () {
+        $(".course-div").hide();
+        console.log("User clicked on Course 6.");
+        $("#output-image6").show();
+    });
+
+    $("#course7").click(function () {
+        $(".course-div").hide();
+        console.log("User clicked on Course 7.");
+        $("#output-image7").show();
+      });
+  });
+
+
+
+
+
+
+
+
